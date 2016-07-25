3b23a5fa5e689562da57f5d558b07379858932c4
Mohsen <m.mehabadi@gmail.com>
2016-07-25 12:21:45 +0430

    add user posts

--- source/scripts/pages/user/userController.js
+++ source/scripts/pages/user/userController.js
@@ -5,12 +5,16 @@
     var _injectParams = ['$scope'];
 
     var _controller = 
+		['$scope', '$rootScope', '$routeParams', 'userService',
+		function ($scope, $rootScope, $routeParams, userService)
 		{
+			$rootScope.pageTitle = "Users Posts | Angular Blog";
 			$rootScope.currentPage = "blog";
 
 			$scope.maxItem = 2;
 			//var blog = this;
 			
+			var data = userService.getUserPosts($routeParams.id);
 
 			data.then(function (data)
 			{				
